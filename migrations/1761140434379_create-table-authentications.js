3bbbea1fd52255f01aa1739b51657ddc4bc39a45
zakymzn <mamurzakynurrokhman@gmail.com>
2025-10-24 00:41:23 +0700

    first commit - membuat fitur registrasi pengguna

---
+++ migrations/1761140434379_create-table-authentications.js
@@ -0,0 +1,12 @@
+export const up = (pgm) => {
+  pgm.createTable("authentications", {
+    token: {
+      type: "TEXT",
+      notNull: true,
+    },
+  });
+};
+
+export const down = (pgm) => {
+  pgm.dropTable("authentications");
+};
