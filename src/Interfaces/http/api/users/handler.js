32b000935d1320e2166ef472f9ff9a508f21f581
zakymzn <mamurzakynurrokhman@gmail.com>
2025-10-29 21:35:55 +0700

    menerjemahkan domain error ke http error

--- src/Interfaces/http/api/users/handler.js
+++ src/Interfaces/http/api/users/handler.js
@@ -1,4 +1,6 @@
 const AddUserUseCase = require('../../../../Applications/use_case/AddUserUseCase');
+const ClientError = require('../../../../Commons/exceptions/ClientError');
+const DomainErrorTranslator = require('../../../../Commons/exceptions/DomainErrorTranslator');
 
 class UsersHandler {
   constructor(container) {
@@ -8,7 +10,8 @@ class UsersHandler {
   }
 
   async postUserHandler(request, h) {
-    const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
+    
+      const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
     const addedUser = await addUserUseCase.execute(request.payload);
 
     const response = h.response({
